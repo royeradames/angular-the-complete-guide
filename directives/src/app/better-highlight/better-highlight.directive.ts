69339b3dbe43380ad0d4bbafa26ede18d82bef80
Royer Adames <royeraadames@gmail.com>
2022-01-09 16:58:41 -0500

    📦using HostBinding to bind to host properties

--- directives/src/app/better-highlight/better-highlight.directive.ts
+++ directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,6 +1,7 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   OnInit,
   Renderer2,
@@ -18,6 +19,13 @@ import {
   - https://angular.io/api/core/Renderer2
  */
 export class BetterHighlightDirective implements OnInit {
+  /* bind to the element that uses this directive directly and change its style
+    List of things you can bind to with an html element
+      - https://www.w3schools.com/jsref/dom_obj_all.asp
+    - Shorter syntax for than renderer
+    */
+  @HostBinding("style.backgroundColor") backgroundColor: string = "purple";
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
   ngOnInit(): void {
     /* Implement this callback to set a CSS style for an element in the DOM.
@@ -26,12 +34,13 @@ export class BetterHighlightDirective implements OnInit {
       - @param value — The new value.
       - @param flags — Flags for style variations. No flags are set by default.
     */
-    this.renderer.setStyle(
-      // cannot pass down the wrapper, you need to pass down the actual element
-      this.elRef.nativeElement,
-      "background-color",
-      "blue"
-    );
+    // this.renderer.setStyle(
+    //   // cannot pass down the wrapper, you need to pass down the actual element
+    //   this.elRef.nativeElement,
+    //   "background-color",
+    //   "blue"
+    // );
+    // this.backgroundColor = "blue";
     this.renderer.setStyle(this.elRef.nativeElement, "padding", "1rem");
     this.renderer.setStyle(this.elRef.nativeElement, "color", "white");
   }
@@ -39,15 +48,17 @@ export class BetterHighlightDirective implements OnInit {
   /* listen to events with HostListener decorator */
   // simulate on hover
   @HostListener("mouseenter") mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, "background-color", "red");
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", "red");
+    this.backgroundColor = "red";
   }
   @HostListener("mouseleave") mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      // cannot pass down the wrapper, you need to pass down the actual element
-      this.elRef.nativeElement,
-      "background-color",
-      "blue"
-    );
+    // this.renderer.setStyle(
+    //   // cannot pass down the wrapper, you need to pass down the actual element
+    //   this.elRef.nativeElement,
+    //   "background-color",
+    //   "blue"
+    // );
+    this.backgroundColor = "blue";
     this.renderer.setStyle(this.elRef.nativeElement, "padding", "1rem");
     this.renderer.setStyle(this.elRef.nativeElement, "color", "white");
   }
